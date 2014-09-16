41618337f1d4e1541540c38773be891de27a83d4
mactkg <mactkg@gmail.com>
2014-09-16 16:40:43 +0900

    update migrationfile

--- db/migration/002_create_method.rb
+++ db/migration/002_create_method.rb
@@ -1,16 +1,18 @@
 Sequel.migration do
     transaction
     up do
-        create_table(:method, :ignore_index_errors=>true) do
+        reate_table(:method, :ignore_index_errors=>true) do
             primary_key :id, :type=>Bignum
             String :name, :size=>256
-            Integer :javadoc_id
+            foreign_key :javadoc_id, :javadoc, :type=>Bignum, :null=>false, :key=>[:id]
             String :class, :size=>512
             String :params, :size=>512, :null=>false
             String :return, :size=>128, :null=>false
             File :description
             File :permalink
             File :sample_code
+
+            index [:javadoc_id], :name=>:method_javadoc
             index [:return], :name=>:return
         end
     end
