cccd7ef48ba101852cffcdc94a504c95c22a65e3
mactkg <mactkg@gmail.com>
2014-09-16 17:13:16 +0900

    add keys

--- db/migration/002_create_method.rb
+++ db/migration/002_create_method.rb
@@ -1,18 +1,20 @@
 Sequel.migration do
     transaction
     up do
-        reate_table(:method, :ignore_index_errors=>true) do
+        create_table(:method, :ignore_index_errors=>true) do
             primary_key :id, :type=>Bignum
             String :name, :size=>256
             foreign_key :javadoc_id, :javadoc, :type=>Bignum, :null=>false, :key=>[:id]
-            String :class, :size=>512
-            String :params, :size=>512, :null=>false
+            String :class, :size=>256
+            String :params, :size=>256, :null=>false
             String :return, :size=>128, :null=>false
             File :description
             File :permalink
             File :sample_code
 
             index [:javadoc_id], :name=>:method_javadoc
+            index [:params], :name=>:params
+            index [:class], :name=>:class
             index [:return], :name=>:return
         end
     end
