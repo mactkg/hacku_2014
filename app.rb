24fd88e67905bfbf272e0675261be4ceb7581e98
mactkg <mactkg@gmail.com>
2014-09-17 15:05:40 +0900

    added vague api

--- app.rb
+++ app.rb
@@ -1,7 +1,57 @@
 require 'sinatra/base'
-
+require 'sinatra/json'
+require_relative 'model'
+require 'pp'
 class Joogle < Sinatra::Base
     get '/' do
-      '{"message": "hello, world"}'
+        "Welcome to joogle!"
+    end
+
+
+### API
+    get '/api/test' do
+        res = {
+            packages: [
+                {
+                    name: "twitter4j",
+                    reference: "http://twitter4j.org/javadoc/",
+                },
+                {
+                    name: "Processing",
+                    reference: "http://processing.org/reference/javadoc/core/",
+                },
+            ],
+            methods: [
+                {
+                    name: "background",
+                    params: [
+                        {
+                            name: "rgb",
+                            type: "int",
+                        }
+                    ],
+                    return: "float",
+                    javadoc_name: "Processing",
+                    reference: "http://processing.org/reference/javadoc/core/processing/core/PGraphics.html%23brightness(int)",
+                    description: "( begin auto-generated from brightness.xml ) Extracts the brightness value from a color. ( end auto-generated )",
+                    class: {
+                        package: "processing.core",
+                        name: "processing.core.PGraphics",
+                        simple_name: "PGraphics",
+                        type: "Class",
+                    }
+                }
+            ],
+        }
+       json res
+    end
+
+    get '/api/jdoc' do
+        jdoc = Javadoc.all
+        jdoc.to_json
+    end
+
+    get '/api/search' do
+        params.to_json
     end
 end
