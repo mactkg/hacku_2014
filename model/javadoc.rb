f7df0a41485fe816e5b3c84a5c1bd331814fb550
mactkg <mactkg@gmail.com>
2014-09-18 21:11:16 +0900

    relation and helper methods

--- model/javadoc.rb
+++ model/javadoc.rb
@@ -1,7 +1,9 @@
 require 'sequel'
+require_relative 'method'
 
 module Joogle
     class Javadoc < Sequel::Model(:javadoc)
         plugin :json_serializer
+        one_to_many :method, :class => "Joogle::Method"
     end
 end
--- model/method.rb
+++ model/method.rb
@@ -1,8 +1,44 @@
 require 'sequel'
+require 'uri'
+require_relative 'javadoc'
 
 #Sequel::Model.plugin(:schema)
 module Joogle
     class Method < Sequel::Model(:method)
-        plugin :json_serializer
+        #plugin :json_serializer
+        many_to_one :javadoc, :class => "Joogle::Javadoc"
+        def to_hash
+            params_json = [];
+            params.split(',').each do |param|
+                vk = param.split(' ')
+                params_json << {vk[1] => vk[0]}
+            end
+            {
+                name: name,
+                params: params_json,
+                out: out,
+                javadoc_name: javadoc.name,
+                reference: URI.unescape(permalink),
+                description: description,
+                belong_to: {
+                    name: belong_to,
+                    package: package,
+                    simple_name: simple_name,
+                    type: type,
+                },
+            }
+        end
+
+        def package
+            return belong_to.split(' ')[1].split('.').select{|s| s =~ /^[a-z]/}.join('.')
+        end
+
+        def simple_name
+            return belong_to.split(' ')[1].split(',').last
+        end
+
+        def type
+            return belong_to.split(' ')[0]
+        end
     end
 end
