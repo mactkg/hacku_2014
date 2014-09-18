8b471141db9064f3402ffdbf60e758dabe158f54
mactkg <mactkg@gmail.com>
2014-09-18 16:05:54 +0900

    fix typo

--- model/method.rb
+++ model/method.rb
@@ -2,7 +2,7 @@ require 'sequel'
 
 #Sequel::Model.plugin(:schema)
 module Joogle
-    class Mehtod < Sequel::Model(:method)
+    class Method < Sequel::Model(:method)
         plugin :json_serializer
     end
 end
